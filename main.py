258b0f11cb23124898559abd22932c18589d147b
erisot <erika20062018@gmail.com>
2024-11-06 19:22:12 +0100

    aún queda

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ def resto(num1,num2):
 def incremento2(num1):
     if num1 < 0:
         num1 = num1 + 1
-        print("Número incrementado correctamete ", num1)
+        print("Correctly incremented number ", num1)
     else:
         num1 = num1 - 1
-        print("El número disminuyó correctamente ", num1)
+        print("The number decreased correctly ", num1)
 def PareImpar(n):
     for i in range(0, n +1):
         n = i
         result = n
         if n % 2 == 0:
-            print("El ",  n ," es par")
+            print("The ",  n ," is even")
         else:
-            print("El " , n ," es impar")
+            print("The " , n ," is odd")
     return result
 
 while(True):
-    opcion = int(input("Elige una de estas opciones (1,2,3): "))
+    opcion = int(input("Choose one of these options (1,2,3): "))
     if opcion == 1:
-        num1 = int(input("Ingresa un dividendo: "))
-        num2 = int(input("Ingresa un divisor: "))
-        print("Cociente = ", cociente(num1,num2))
-        print("Resto = ", resto(num1,num2))
+        num1 = int(input("Enter a dividend: "))
+        num2 = int(input("Enter a divisor: "))
+        print("Quotient = ", cociente(num1,num2))
+        print("Rest = ", resto(num1,num2))
     elif opcion == 2:
-        num1 = int(input("Ingresa un número: "))
+        num1 = int(input("Enter a number: "))
         incremento2(num1)
     elif opcion == 3:
-        num1 = int(input("Ingrese un número: "))
+        num1 = int(input("Enter a number: "))
         PareImpar(num1)
     else:
         break    
